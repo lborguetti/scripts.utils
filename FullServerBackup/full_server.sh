2126540904b0d5cb3fd579853e66f2289080f9f4
Luciano Antonio Borguetti Faustino <luciano.faustino@digitro.com.br>
2014-08-21 14:27:06 -0300

    Update full_server.sh

--- FullServerBackup/full_server.sh
+++ FullServerBackup/full_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir /backups
+mkdir -p /backups
+cd /
 tar -cvzpf /backups/full_server.tar.gz --directory=/ --exclude=proc --exclude=sys --exclude=dev/pts --exclude=backups . > /dev/null 2>&1
 
